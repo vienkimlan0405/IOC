b4c3c3565f5dd5f7badd9f8bff420a400df0805a
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-28 22:01:45 +0700

    Create ValueDemoController with @Value injection
    Add ValueDemoController to demonstrate @Value usage

---
+++ Value/src/main/resources/4. @Value từ application.properties/Demo.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ValueDemoController {
+
+    // Bài 4: Inject app.name
+    @Value("${app.name}")
+    private String appName;
+
+    @GetMapping("/value-demo")
+    public String getValueDemo() {
+        System.out.println("App Port: " + appPort);
+        
+        return String.format(
+            "Name: %s | Description: %s | Roles: %s | Port: %d",
+            appName, appDescription, roles, appPort
+        );
+    }
+}
