b3bb8c40da754d075b6f264b3e6a702f41215fc8
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-28 21:48:50 +0700

    Create Student.java

---
+++ 1. Tạo Bean cơ bản/Student.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+    public String getInfo() {
+        return "Student: Nguyễn Văn A";
+    }
+}
