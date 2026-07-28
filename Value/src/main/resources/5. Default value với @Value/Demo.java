dac5b9c6b304f4ec92f33e1eb988b6571bfc21f8
vienkimlan0405 <vienkimlan0405@gmail.com>
2026-07-28 22:02:38 +0700

    Create Demo.java

---
+++ Value/src/main/resources/5. Default value với @Value/Demo.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ValueDemoController {
+    // Bài 5: Giá trị mặc định nếu không khai báo config
+    @Value("${app.description:Default Description}")
+    private String appDescription;
+
+    @GetMapping("/value-demo")
+    public String getValueDemo() {
+        System.out.println("App Port: " + appPort);
+        
+        return String.format(
+            "Name: %s | Description: %s | Roles: %s | Port: %d",
+            appName, appDescription, roles, appPort
+        );
+    }
+}
